684283f536255a8db8bd88d7680040478622f494
rybakov.a <andrey374@mail.ru>
2020-08-26 08:51:09 +0300

    cases 1 and 2 done

---
+++ Cases.Domain.Contracts/IYouTubeLikeService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Cases.Domain.Contracts
+{
+    public interface IYouTubeLikeService
+    {
+        Task<bool> DoTests(int id);
+    }
+}
---
+++ Cases/Controllers/CasesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Cases.Domain.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Cases.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class CasesController : ControllerBase
+    {
+        private readonly ILogger<CasesController> _logger;
+        private readonly IYouTubeLikeService _youTubeLikeService;
+
+        public CasesController(ILogger<CasesController> logger, IYouTubeLikeService youTubeLikeService)
+        {
+            _logger = logger;
+            _youTubeLikeService = youTubeLikeService;
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> Case(int id)
+        {
+            _youTubeLikeService.DoTests(id);
+
+            return Ok();
+        }
+    }
+}
